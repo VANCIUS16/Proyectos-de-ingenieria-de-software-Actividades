36a2a496e9b501e2d376eac74227c82ea470baf0
JoelHernandez <joelhernandez22@outlook.com>
2022-03-14 13:28:23 -0700

    commit para actividad 4 proyectos de software

--- Actividad3_2.py
+++ Actividad3_2.py
@@ -1,16 +1,14 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Feb  4 20:36:04 2022
-
-@author: marcn
-"""
 from bs4 import BeautifulSoup
 from pathlib import Path
 import os
 import time
 
 # path del archivo
-directory = "Files"
+directory = "D:/joelh/Documents/TecMilenio/ProyectosIngenieriaSoft/Files"
+# logs
+log_abrir = open("log_abrir.txt", 'w+')
+log_remove = open("log_remove", 'w+')
+log_diccionario = open("log_diccionario", 'w+')
 
 
 # funcion para extraer texto
@@ -24,46 +22,61 @@ def removetags(html):
     return file_text
 
 
+# Actividad 1: leer archivos html
+Start1 = time.time()
+for filename in os.listdir(directory):
+    Start = time.time()
+    f = open(os.path.join(directory, filename), 'r')
+    f.close()
+    end = time.time()
+    log_abrir.write("File Name: " + filename + " || Times: " + str(end - Start) + '\n')
+End1 = time.time()
+log_abrir.write("Tiempo total: " + str(End1 - Start1))
+
+# Actividad 2: remover html tags
+Start2 = time.time()
 for fileName in os.listdir(directory):
     Start = time.time()
-    
-    path2 = directory+"/"+fileName
+
+    path2 = "D:/joelh/Documents/TecMilenio/ProyectosIngenieriaSoft/Files/" + fileName
     # Creamos un archivo txt
-    newFilePath = "NewFiles2/"+fileName
+    newFilePath = "D:/joelh/Documents/TecMilenio/ProyectosIngenieriaSoft/NewFiles2/" + fileName
     f = open(newFilePath, "w+", encoding='utf-8', errors='ignore')
-   
+
     # Escribimos el texto en el nuevo documento
     f.write(removetags(path2))
     f.close()
-    #p = Path(newFilePath)
-    #p.rename(p.with_suffix('.txt'))
+    p = Path(newFilePath)
+    p.rename(p.with_suffix('.txt'))
     end = time.time()
-#    print("File Name: "+fileName+" || Times: ", end-Start)
-
+    log_remove.write("File Name: " + fileName + " || Times: " + str(end - Start) + '\n')
+End2 = time.time()
+log_remove.write("Tiempo total: " + str(End2 - Start2))
 
 # Actividad 3. Extraer palabras de archivos HTML
-pathClean = "NewFiles2/"
-    
+pathClean = "D:/joelh/Documents/TecMilenio/ProyectosIngenieriaSoft/NewFiles2/"
+
+Start3 = time.time()
 for name in os.listdir(pathClean):
     Start = time.time()
-    oldPath = "NewFiles2/" + name
-    newPath = "Diccionario/" + name
+    oldPath = "D:/joelh/Documents/TecMilenio/ProyectosIngenieriaSoft/NewFiles2/" + name
+    newPath = "D:/joelh/Documents/TecMilenio/ProyectosIngenieriaSoft/Diccionario/" + name
     file_Sorted = open(newPath, "w+", encoding='utf-8', errors='ignore')
     # Abrimos el .txt en read
     f = open(oldPath, "r", encoding='utf-8', errors='ignore')
 
     # Declaramos una lista
     words = []
-    
+
     # Iteramos por cada linea del documento de texto
     for line in f:
         # iteramos por cada palabra obtenida al dividir la linea
         for word in line.split():
             # Confirmamos que la palabra sea unica y agreamos a la lista
-            # if word not in words:
-            words.append(word)
+            if word not in words:
+                words.append(word)
     f.close()
-    
+
     # Ordenamos la lista
     words_Sorted = sorted(words, key=str.lower)
     # Ingresamos la lista al nuevo documento
@@ -73,6 +86,8 @@ for name in os.listdir(pathClean):
     file_Sorted.close()
     p = Path(newPath)
     p.rename(p.with_suffix('.txt'))
-    
+
     end = time.time()
-# print("File Name: "+name+" || Times: ", end-Start)
+    log_diccionario.write("File Name: " + name + " || Times: " + str(end - Start) + '\n')
+End3 = time.time()
+log_diccionario.write("Tiempo total: " + str(End3 - Start3))
---
+++ Actividad4/Actividad 4.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+import os
+import time
+
+
+directory = Path("D:\PycharmProjects\Proyectos-de-ingenieria-de-software-Actividades\Actividad 3\Diccionario")
+log_diccionario = open("log_diccionarioCompleto", 'w+')
+# Declaramos una lista
+words = []
+for name in os.listdir(directory):
+    Start = time.time()
+    FilePath = directory / name
+    f = open(FilePath, "r", encoding='utf-8', errors='ignore')
+
+    # Iteramos por cada linea del documento de texto
+    for line in f:
+        # iteramos por cada palabra obtenida al dividir la linea
+        for word in line.split():
+            # Confirmamos que la palabra sea unica y agreamos a la lista
+            if word.isalpha():
+                words.append(word)
+    f.close()
+
+file_Sorted = open("DiccionarioCompleto", "w+", encoding='utf-8', errors='ignore')
+# Ordenamos la lista
+words_Sorted = sorted(words, key=str.lower)
+for w in words_Sorted:
+    file_Sorted.write(w)
+    file_Sorted.write('\n')
+file_Sorted.close()
+End = time.time()
+log_diccionario.write("Tiempo total: " + str(End - Start))
