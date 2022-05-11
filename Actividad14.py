f97c0f4f752158a066240ad1fdac8e378986f866
VANCIUS16 <67486850+VANCIUS16@users.noreply.github.com>
2022-05-11 20:43:46 +0200

    Actividad 14

---
+++ Actividad14.py
@@ -0,0 +1,13 @@
+#!D:\Users\marcn\AppData\Local\Programs\Python\Python310\python.exe
+
+title = 'Actividad 14'
+print("Content-type:text/html\r\n\r\n")
+print('<html>')
+print('<head>')
+print('<title>', title, '</title>')
+print('<h2>', title, '</h2>')
+print('</head>')
+print('<body>')
+print('<h2>Hola Mundo</h2>')
+print('</body>')
+print('</html>')
